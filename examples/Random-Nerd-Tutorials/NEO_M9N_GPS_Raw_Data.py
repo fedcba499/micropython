99fbe6063a0817cb8dbfca2f737d1766cc9ae970
fedcba499 <fedcba4991@gmail.com>
2026-02-03 16:45:20 +0200

    gps button

---
+++ examples/Random-Nerd-Tutorials/NEO_M9N_GPS_Raw_Data.py
@@ -0,0 +1,15 @@
+import machine
+from time import sleep
+
+# Define the UART pins and create a UART object
+gps_serial = machine.UART(1, baudrate=38400, tx=4, rx=5)
+
+while True:
+    if gps_serial.any():
+        line = gps_serial.readline()  # Read a complete line from the UART
+        if line:
+            line = line.decode('utf-8')
+            print(line.strip())
+    sleep(0.5)
+
+    
---
+++ examples/gsw-micropython-on-raspberry-pi-pico-2e/ch04/Button_Up.py
@@ -0,0 +1,10 @@
+import machine
+import time
+
+button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+
+while True:
+    if button.value() == 0:
+        print("You pressed the button!")
+        time.sleep(2)
+        
